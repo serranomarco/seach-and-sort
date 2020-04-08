075435e5847aa1eb0df601aa518612e2980e0445
Zachary Henderson <zachary.henderson114@gmail.com>
2020-04-08 11:38:30 -0500

    finished merge

--- merge/lib/merge_sort.js
+++ merge/lib/merge_sort.js
@@ -1,34 +1,63 @@
 function merge(array1, array2) {
-  // var result as array
-  // while ( a and b have elements )
-  //   if ( a[0] > b[0] )
-  //     add b[0] to the end of result
-  //     remove b[0] from b
-  //   else
-  //     add a[0] to the end of result
-  //     remove a[0] from a
-  //   end if
-  // end while
-  // while ( a has elements )
-  //   add a[0] to the end of result
-  //   remove a[0] from a
-  // end while
-  // while ( b has elements )
-  //   add b[0] to the end of result
-  //   remove b[0] from b
-  // end while
-  // return result
+  let result = [];
+
+  while (array1.length !== 0 && array2.length !== 0) {
+    const value1 = array1[0];
+    const value2 = array2[0];
+    if (value1 > value2) {
+      result.push(array2.shift());
+    } else {
+      result.push(array1.shift());
+    }
+  }
+
+  if (array1.length !== 0) {
+    result = [...result, ...array1];
+  }
+
+  if (array2.length !== 0) {
+    result = [...result, ...array2];
+  }
+
+  return result;
 }
+// var result as array
+// while ( a and b have elements )
+//   if ( a[0] > b[0] )
+//     add b[0] to the end of result
+//     remove b[0] from b
+//   else
+//     add a[0] to the end of result
+//     remove a[0] from a
+//   end if
+// end while
+// while ( a has elements )
+//   add a[0] to the end of result
+//   remove a[0] from a
+// end while
+// while ( b has elements )
+//   add b[0] to the end of result
+//   remove b[0] from b
+// end while
+// return result
 
 function mergeSort(array) {
-  // if ( n == 1 ) return a
-  // /* Split the array into two */
-  // var l1 as array = a[0] ... a[n/2]
-  // var l2 as array = a[n/2+1] ... a[n]
-  // l1 = mergesort( l1 )
-  // l2 = mergesort( l2 )
-  // return merge( l1, l2 )
+  if (array.length <= 1) return array;
+
+  let leftArr = array.slice(0, Math.ceil(array.length / 2));
+  let rightArr = array.slice(Math.ceil(array.length / 2));
+  leftArr = mergeSort(leftArr);
+  rightArr = mergeSort(rightArr);
+
+  return merge(leftArr, rightArr);
 }
+// if ( n == 1 ) return a
+// /* Split the array into two */
+// var l1 as array = a[0] ... a[n/2]
+// var l2 as array = a[n/2+1] ... a[n]
+// l1 = mergesort( l1 )
+// l2 = mergesort( l2 )
+// return merge( l1, l2 )
 
 module.exports = {
   merge,
